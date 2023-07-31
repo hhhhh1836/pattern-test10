b123795d1f7e0335778e9cebc0f711c07d61075a
hhhhh1836 <64543684+hhhhh1836@users.noreply.github.com>
2023-07-31 02:49:28 -0700

    Update pattern.js
    probably doesnt like creating a canvas so i'll just try something simple

--- pattern.js
+++ pattern.js
@@ -9,28 +9,12 @@ window.pattern = {
     smoothGraphics, // Function that returns a new SmoothGraphics() from @pixi/graphics-smooth:0.0.30
     random // Function returns pseudorandom number between 0 and 1. Seed changes daily.
   ) => {
-    let container = new pixi.Container();
-    let canvas = document.createElement("CANVAS");
-    let ctx = canvas.getContext("2d");
-    ctx.width = width * tileSize;
-    ctx.height = height * tileSize;
-    canvas.width = ctx.width;
-    canvas.height = ctx.height;
-    let midX = (width * tileSize) / 2;
-    let midY = (height * tileSize) / 2;
-    let gradient = ctx.createRadialGradient(midX, midY, 0, midX, midY, Math.min(midX, midY));
-    gradient.addColorStop(0, "black");
-    gradient.addColorStop(0.5, "white");
-    gradient.addColorStop(1, "black");
-    ctx.fillStyle = gradient;
-    ctx.fillRect(0, 0, ctx.width, ctx.height);
-    console.log(canvas);
-    let tex = pixi.Texture.from(canvas);
-    let sprite = new pixi.Sprite(tex);
-    sprite.position.set(tileSize, tileSize);
-    sprite.width = width * tileSize;
-    sprite.height = height * tileSize;
-    container.addChild(sprite);
+    const container = new pixi.Container();
+    const graphics = new pixi.Graphics();
+    graphics.beginFill(0xffffff);
+    graphics.drawRect(tileSize, tileSize, (width / 2 + 1) * tileSize, (height / 2) + 1 * tileSize);
+    graphics.endFill();
+    container.addChild(graphics);
     return container;
   },
 };
